38afb881da8b31488360dbb02dbdf62608732bc6
Mohammed Shadman <mohammedshadman101@gmail.com>
2026-06-10 23:40:57 +0530

    Update db.py

--- models/db.py
+++ models/db.py
@@ -1,7 +1,7 @@
 from supabase import create_client
 from config import SUPABASE_URL, SUPABASE_KEY
 
-print("SUPABASE_URL =", SUPABASE_URL)
+print("SUPABASE_URL =", repr(SUPABASE_URL))
 print("SUPABASE_KEY EXISTS =", bool(SUPABASE_KEY))
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
